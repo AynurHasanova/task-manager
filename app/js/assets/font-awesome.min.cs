c23b519e623f7d85534422bc116e1f84cb7d5adc
Aynur Hasanova <hesenova.aynur@gmail.com>
2020-05-10 17:52:18 +0100

    include assets

---
+++ app/js/assets/font-awesome.min.cs
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>8719DCDA70359115</RequestId><HostId>g6RM6bmDWYjF08tjTCBJgopJSyi0BiuPGnldH87jZa2ssnH2QDaAlfOoCv2A7oHCSYjRsVDzJyg=</HostId></Error>
